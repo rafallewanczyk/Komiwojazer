63de13723ad5885ed1aa508254977387648d82b1
rlewancz <rlewancz@mion.elka.pw.edu.pl>
2019-11-28 11:26:08 +0100

    implemented search space genererating algorithm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from pygraph import graph_to_dot
 from pygraph.classes.undirected_graph import UndirectedGraph
 from algorithms.brute_force import BruteForce
+from algorithms.a_star import Astar
 from algorithms.astar import *
 from heapq import *
 visited = 10 * [0]
@@ -8,21 +10,15 @@ visited = 10 * [0]
 def main():
     graph = UndirectedGraph()
 
-    for i in range(1, 8): graph.new_node()
-    graph.new_edge(1, 2, 8)
-    graph.new_edge(1, 3, 4)
-    graph.new_edge(1, 4, 5)
-    graph.new_edge(2, 5, 4)
-    graph.new_edge(2, 7, 12)
-    graph.new_edge(3, 5, 8)
-    graph.new_edge(3, 7, 19)
-    graph.new_edge(3, 6, 9)
-    graph.new_edge(4, 7, 99)
-    graph.new_edge(4, 6, 7)
-    graph.new_edge(5, 7, 11)
-    graph.new_edge(6, 7, 7)
+    for i in range(1, 5): graph.new_node()
+    graph.new_edge(1, 2, 2)
+    graph.new_edge(2, 3, 3)
+    graph.new_edge(1, 3, 9)
+    graph.new_edge(3, 4, 7)
+    #BruteForce().run(graph, 1)
+    Astar().generate_search_space(graph, 1)
+    #Astar().run(graph, 1)
 
-    BruteForce().run(graph, 1)
 
 def dfs(graph, start):
     visited[start] = 1
