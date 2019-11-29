b73e072418331f0e5b346740bda884d567f62b71
kejbi <biegajski.kacper@gmail.com>
2019-11-29 13:21:09 +0100

    graph manager implemented + tested

---
+++ data/graph_manager.py
@@ -0,0 +1,46 @@
+import networkx as nx
+from math import sqrt
+class GraphManager():
+    
+    def read_from_file(self, nodes, links):
+        graph = nx.Graph()
+        with open(nodes, "r") as nodes_file:
+            for line in nodes_file.readlines()[1:-1]:
+                words = line.split()
+                graph.add_node(words[0], x=float(words[2]), y=float(words[3]))
+        
+        with open(links, "r") as links_file:
+            for line in links_file.readlines()[1:-1]:
+                link = line.split()
+                node_x = link[2] # city 1
+                node_y = link[3] # city 2
+                
+                graph.add_edge(node_x, node_y, weight = self.distance(graph.nodes[node_x], graph.nodes[node_y]))
+                
+
+        return graph
+
+
+    # x - node X of graph
+    # y - node Y of graph
+    def distance(self, x, y):
+        ax = x["x"]
+        ay = x["y"]
+        bx = y["x"]
+        by = y["y"]
+        return sqrt((ax-bx)**2 + (ay-by)**2)
+
+    def positions(self, graph):
+        posistion_dict = {}
+
+        for node in graph.nodes:
+            x = graph.nodes[node]["x"]
+            y = graph.nodes[node]["y"]
+            posistion_dict[node] = (x,y)
+            
+        return posistion_dict
+
+
+
+
+
