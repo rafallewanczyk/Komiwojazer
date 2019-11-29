ff958ed2e6710700b2d9b43cbd03cfb265c1c0ab
kejbi <biegajski.kacper@gmail.com>
2019-11-29 14:52:59 +0100

    Drawing with labels

--- data/graph_drawer.py
+++ data/graph_drawer.py
@@ -15,3 +15,10 @@ class GraphDrawer():
         nx.draw_networkx(graph, pos = positions)
         nx.draw_networkx_edges(graph, pos=positions, edgelist=edges, edge_color="red")
         plt.show()
+
+    def draw_with_labels(self, graph):
+        positions = self.manager.positions(graph)
+        nx.draw_networkx(graph, pos = positions)
+        labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw_networkx_edges_labels(graph, pos=positions, edge_labels=labels)
+        plt.show()
