bb9de39cf1e436154a6ffe49a521a21efe03d552
rlewancz <rlewancz@mion.elka.pw.edu.pl>
2019-11-29 14:49:56 +0100

    main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import networkx as net
+from data.graph_manager import GraphManager
 import matplotlib.pyplot as plt
 from algorithms.brute_force import BruteForce
 from algorithms.a_star import Astar
@@ -7,14 +8,16 @@ visited = 10 * [0]
 
 
 def main():
-    graph = net.Graph()
+    manager = GraphManager()
+    graph = manager.read_from_file("data/examples/poland_nodes.txt", "data/examples/poland_links.txt")
 
-    for i in range(1, 5): graph.add_node(i)
-    graph.add_edge(1, 2, weight = 1)
-    graph.add_edge(1, 3, weight = 2)
-    graph.add_edge(2, 3, weight = 1)
-    graph.add_edge(2, 4, weight = 3)
-    graph.add_edge(3, 4, weight = 2)
+   # graph = net.Graph()
+    # for i in range(1, 5): graph.add_node(i)
+    # graph.add_edge(1, 2, weight = 1)
+    # graph.add_edge(1, 3, weight = 2)
+    # graph.add_edge(2, 3, weight = 1)
+    # graph.add_edge(2, 4, weight = 3)
+    # graph.add_edge(3, 4, weight = 2)
 
     #print(list(graph.neighbors(1)))
    # print(graph.get_edge_data(1, 2)['weight'])
@@ -31,9 +34,11 @@ def main():
     # graph.add_edge("1test", "4test", weight = 100)
 
 
-    BruteForce().run(graph, 1)
+    BruteForce().run(graph, "Gdansk")
+    #BruteForce().run(graph, "1test")
     # Astar().generate_search_space(graph, 1)
-    Astar().solve(graph, 1)
+    Astar().solve(graph, "Gdansk")
+    #Astar().solve(graph, "1test")
     # net.draw_networkx(graph, pos=d)
     # plt.show()
     #l = sorted(list(graph.get_all_edge_objects()), key = lambda x : x['cost'])
@@ -42,21 +47,6 @@ def main():
 
     #Astar().run(graph, 1)
 
-
-
-def dfs(graph, start):
-    visited[start] = 1
-    for v in graph.neighbors(start):
-        if visited[graph.nodes[v]['id']] == 0: dfs(graph, graph.nodes[v]['id'])
-
-    print(start)
-
-
-def comp(a, b):
-    if a > b:
-        return True
-    else:
-        return False
 
 
 if __name__ == '__main__':
