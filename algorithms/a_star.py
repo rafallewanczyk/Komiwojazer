3e9f1fb3a2ff914e57bbc864046dace0ba02d36f
rlewancz <rlewancz@mion.elka.pw.edu.pl>
2019-11-29 11:17:30 +0100

    almost done version with pygraph

--- algorithms/a_star.py
+++ algorithms/a_star.py
@@ -6,9 +6,19 @@ class Astar:
    final_cost = 0
    result = []
    
+   def is_on_path(self, path, v1, v2):
+      if abs(path.index(v1) - path.index(v2)) == 1:
+         return 1
+      else:
+         return 0
 
-   def heuristic(self, graph, v1, v2):
-      #print(sorted(list(graph.get_all_edge_objects()), key = lambda x : x['cost']))
+   def heuristic(self, graph, path,  v1, v2):
+      l = sorted(list(graph.get_all_edge_objects()), key = lambda x : x['cost'])
+      for i in l:
+         if self.is_on_path(path, i['vertices'][0], i['vertices'][1]):
+            continue
+         else :
+            return l['cost'] * (graph.num_nodes() - len(path))
       return 0
 
    def generate_search_space(self, graph, start):
@@ -29,9 +39,13 @@ class Astar:
             if v in self.search_space.nodes[id]['data']['path']:
                continue
 
+            if len(self.search_space.nodes[id]['data']['path']) == graph.num_nodes():
+               print(f"{self.search_space.nodes[id]['data']['path']} koszt : {self.search_space.nodes[id]['data']['cost']}")
+               return
+
             new = self.search_space.new_node()
-            self.search_space.new_edge(new, id, graph.edge_cost(v, self.search_space.nodes[id]['data']['path'][-1]) + self.heuristic(graph, new, id))
             self.search_space.nodes[new]['data']['path'] = self.search_space.nodes[id]['data']['path']  + [v]
+            self.search_space.new_edge(new, id, graph.edge_cost(v, self.search_space.nodes[id]['data']['path'][-1]) + self.heuristic(graph, self.search_space.nodes[id]['data']['path'], v, self.search_space.nodes[id]['data']['path'][-1]))
             self.search_space.nodes[new]['data']['cost'] = self.search_space.nodes[id]['data']['cost'] + graph.edge_cost(v, self.search_space.nodes[id]['data']['path'][-1])
             print(f"dodaje {v} ")
             print(self.search_space.nodes[new]['data']['path'])
